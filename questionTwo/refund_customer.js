c1a2fd36a36ea5ad3a19ed07a67d72a6f007bf6d
sajicode <sajioloye@gmail.com>
2021-09-10 02:10:57 +0100

    chore: use try-catch block

--- questionTwo/refund_customer.js
+++ questionTwo/refund_customer.js
@@ -6,26 +6,30 @@ const wallet_url = "https://api.okra.ng/v2/mock-api/fetch-wallet";
 const pay_url = "https://api.okra.ng/v2/mock-api/pay";
 
 async function refundCustomer(company, user, amount) {
-  // fetch user's current wallet
-  const { data } = await axios.post(wallet_url, {
-    id: user,
-  });
+  try {
+    // fetch user's current wallet
+    const { data } = await axios.post(wallet_url, {
+      id: user,
+    });
 
-  const previous_wallet = data.data.wallet;
+    const previous_wallet = data.data.wallet;
 
-  // refund customer
-  const { data: pay_data } = await axios.post(pay_url, {
-    from_id: company,
-    to_id: user,
-    amount,
-  });
+    // refund customer
+    const { data: pay_data } = await axios.post(pay_url, {
+      from_id: company,
+      to_id: user,
+      amount,
+    });
 
-  const recent_wallet = pay_data.data.wallets.to;
+    const recent_wallet = pay_data.data.wallets.to;
 
-  console.log({
-    previous_wallet,
-    recent_wallet,
-  });
+    console.log({
+      previous_wallet,
+      recent_wallet,
+    });
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 refundCustomer("484929849", "573839293", 2003.0);
